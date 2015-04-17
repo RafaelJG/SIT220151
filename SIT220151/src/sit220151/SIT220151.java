63163e08b1d4ce5cab34d8b47b3a4e13761ffefa
maycowmeira <maycowmeira@maycowmeira-VPCEG17FB>
2015-04-17 20:36:20 -0300

    que cachorro oq

--- SIT220151/src/sit220151/SIT220151.java
+++ SIT220151/src/sit220151/SIT220151.java
@@ -16,6 +16,7 @@ public class SIT220151 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("cachorro");
     }
     
 }
