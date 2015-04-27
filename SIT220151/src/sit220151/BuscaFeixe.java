8cb668f5b0ca44aaf29dbb8e03fc630ed1776bfa
rafael <rafael@rafael-vostro-5470>
2015-04-26 23:36:59 -0300

    Domingo 28/04

---
+++ SIT220151/src/sit220151/BuscaFeixe.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sit220151;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author rafael
+ */
+public class BuscaFeixe {
+    
+    
+    public ArrayList<Item> Vizinhos(Mochila mochila){
+        ArrayList <Item> vizinhos  = new ArrayList<Item>();
+        
+        
+        
+        return vizinhos;
+    
+    }
+    
+}
